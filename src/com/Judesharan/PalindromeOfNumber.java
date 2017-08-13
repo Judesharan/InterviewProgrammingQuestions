9c5c7d4d39cad76de8bb7cedc23bed90b23d6f33
Jude Sharan J <judesharan@gmail.com>
2017-08-13 08:57:35 +0530

    Interview Programming Questions

---
+++ src/com/Judesharan/PalindromeOfNumber.java
@@ -0,0 +1,29 @@
+package com.Judesharan;
+
+import java.util.Scanner;
+
+public class PalindromeOfNumber {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number = input.nextInt();
+		System.out.println(number + " is palindrome: " + isPalindrome(number));
+		input.close();
+	}
+
+	public static boolean isPalindrome(int number) {
+
+		int numberInReverse = 0, digit;
+		int originalNumber = number;
+		while (number > 0) {
+			digit = number % 10;
+			numberInReverse = (numberInReverse * 10) + digit;
+			number /= 10;
+		}
+		if (originalNumber == numberInReverse)
+			return true;
+		return false;
+	}
+}
